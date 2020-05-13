bd201da80ef73e730a232cc119bde9139eb5775c
xres-ly <xres331509@gmail.com>
2020-05-13 15:27:14 +0800

    bug

--- app/src/main/java/com/example/rocketcat/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/rocketcat/ui/activity/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.rocketcat.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.rocketcat.R
+import com.google.android.material.dialog.MaterialDialogs
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
