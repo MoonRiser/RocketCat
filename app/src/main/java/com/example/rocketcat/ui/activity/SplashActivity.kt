7a56662466f4c94523079ab4cb8312256b2bf7fb
xres <xres@gmail.com>
2020-05-16 08:41:46 +0800

    add splash activity

--- app/src/main/java/com/example/rocketcat/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/rocketcat/ui/activity/SplashActivity.kt
@@ -1,19 +1,64 @@
 package com.example.rocketcat.ui.activity
 
+import android.animation.ArgbEvaluator
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import com.example.rocketcat.R
+import com.example.rocketcat.adapter.MyGalleryAdapter
 import com.example.rocketcat.base.BaseActivity
 import com.example.rocketcat.base.BaseViewModel
 import com.example.rocketcat.databinding.ActivitySplashBinding
+import com.example.rocketcat.utils.dp2px
 import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : BaseActivity<BaseViewModel, ActivitySplashBinding>() {
 
+    private val imgs = arrayListOf(
+        R.drawable.jump, R.drawable.paint, R.drawable.sit
+    )
+    private val adapter1 = MyGalleryAdapter(imgs)
+    private val values = listOf("#3E44D9", "#3D2161", "#EB8127")
+    private val colors = values.map { Color.parseColor(it) }
+    val evaluator = ArgbEvaluator()
 
     override fun layoutId() = R.layout.activity_splash
 
+
     override fun initView(savedInstanceState: Bundle?) {
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        vp2_welcome.apply {
+            adapter = adapter1
+            offscreenPageLimit = 1
+            setPageTransformer(MarginPageTransformer(dp2px(32f)))
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageScrolled(
+                    position: Int,
+                    positionOffset: Float,
+                    positionOffsetPixels: Int
+                ) {
+                    if (position < colors.size - 1) {
+                        val color =
+                            evaluator.evaluate(
+                                positionOffset,
+                                colors[position],
+                                colors[position + 1]
+                            ) as Int
+                        img_bg.setBackgroundColor(color)
+                        bt_skip.visibility = View.GONE
+                    } else {
+                        bt_skip.visibility = View.VISIBLE
+                    }
+
+                }
+            })
+        }
 
         bt_skip.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
---
+++ app/src/main/java/com/example/rocketcat/ui/fragment/Tab2Fragment.kt
@@ -0,0 +1,22 @@
+package com.example.rocketcat.ui.fragment
+
+import android.os.Bundle
+import com.example.rocketcat.R
+import com.example.rocketcat.base.BaseFragment
+import com.example.rocketcat.base.BaseViewModel
+import com.example.rocketcat.databinding.FragmentTab2Binding
+
+
+class Tab2Fragment : BaseFragment<BaseViewModel,FragmentTab2Binding>() {
+
+    override fun layoutId() = R.layout.fragment_tab2
+
+    override fun initView(savedInstanceState: Bundle?) {
+
+    }
+
+    override fun createObserver() {
+
+    }
+
+}
