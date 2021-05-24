a9958d4126958733eee205bbe8a7776414d54d99
xres <xres331509@gmail.com>
2021-05-25 01:17:25 +0800

    add gravity sensor

--- app/src/main/java/com/example/rocketcat/ui/fragment/DashboardFragment.kt
+++ app/src/main/java/com/example/rocketcat/ui/fragment/DashboardFragment.kt
@@ -1,6 +1,11 @@
 package com.example.rocketcat.ui.fragment
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import android.util.Log
 import android.view.MotionEvent
@@ -9,19 +14,20 @@ import android.view.View
 import android.widget.Toast
 import androidx.dynamicanimation.animation.DynamicAnimation
 import androidx.dynamicanimation.animation.SpringAnimation
-import androidx.dynamicanimation.animation.SpringForce
-import com.example.rocketcat.R
-import com.example.rocketcat.adapter.MyGalleryAdapter
 import com.example.common.base.BaseFragment
 import com.example.common.base.BaseViewModel
 import com.example.common.ext.createSpringAnimation
+import com.example.rocketcat.R
+import com.example.rocketcat.adapter.MyGalleryAdapter
 import com.example.rocketcat.customview.transformer.CarouselPageTransformer
 import com.example.rocketcat.customview.transformer.HorizontalStackTransformer
 import com.example.rocketcat.databinding.FragmentDashBoardBinding
 import java.lang.Math.toDegrees
+import kotlin.math.absoluteValue
 import kotlin.math.atan2
 
-class DashboardFragment : BaseFragment<BaseViewModel, FragmentDashBoardBinding>() {
+class DashboardFragment : BaseFragment<BaseViewModel, FragmentDashBoardBinding>(),
+    SensorEventListener {
 
     private val imgs = listOf(
         R.drawable.ads,
@@ -32,17 +38,19 @@ class DashboardFragment : BaseFragment<BaseViewModel, FragmentDashBoardBinding>(
         R.drawable.paint,
         R.drawable.sit
     )
-    private val adapter1: MyGalleryAdapter = MyGalleryAdapter(imgs)
-    private val adapter2: MyGalleryAdapter = MyGalleryAdapter(imgs)
-    private var xDiffLeft: Float = 0f
-    private var yDiffTop: Float = 0f
-    private var isMoved = false
+
+    private val sensorManager by lazy { context?.getSystemService(Context.SENSOR_SERVICE) as SensorManager }
+    private val sensor: Sensor? by lazy {
+        sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
+    }
+
 
     private lateinit var scaleGestureDetector: ScaleGestureDetector
 
     companion object {
         const val INITIAL_SCALE = 1f
         const val INITIAL_ROTATION = 0F
+        const val BALANCE = 0.7F
     }
 
     override fun layoutId() = R.layout.fragment_dash_board
@@ -50,13 +58,13 @@ class DashboardFragment : BaseFragment<BaseViewModel, FragmentDashBoardBinding>(
     override fun initView(savedInstanceState: Bundle?) {
 
         binding.vp2Stack.apply {
-            adapter = adapter1
+            adapter = MyGalleryAdapter(imgs)
             offscreenPageLimit = 3
             setPageTransformer(HorizontalStackTransformer(this))
 
         }
         binding.vp2Carou.apply {
-            adapter = adapter2
+            adapter = MyGalleryAdapter(imgs)
             offscreenPageLimit = 3
             currentItem = 2
             setPageTransformer(CarouselPageTransformer())
@@ -67,12 +75,32 @@ class DashboardFragment : BaseFragment<BaseViewModel, FragmentDashBoardBinding>(
 
     }
 
+    private var xDiffLeft: Float = 0f
+    private var yDiffTop: Float = 0f
+    private var isMoved = false
+
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    private lateinit var anim1X: SpringAnimation
+    private lateinit var anim1Y: SpringAnimation
+
     @SuppressLint("ClickableViewAccessibility")
     private fun initFabSpringAnimation() {
 
-        val (anim1X, anim1Y) = binding.fab.run {
+        binding.fab.run {
             createSpringAnimation(SpringAnimation.TRANSLATION_X) to
                     createSpringAnimation(DynamicAnimation.TRANSLATION_Y)
+        }.let {
+            anim1X = it.first
+            anim1Y = it.second
         }
         binding.fab.setOnClickListener {
             Toast.makeText(requireContext(), "look carefully", Toast.LENGTH_LONG).show()
@@ -176,6 +204,30 @@ class DashboardFragment : BaseFragment<BaseViewModel, FragmentDashBoardBinding>(
             true
         }
 
+
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_GRAVITY) {
+
+            if (anim1X.isRunning) return
+            val xt = event.values[0] / -10f
+            val yt = event.values[1] / 10f
+            Log.i("xres", "x:$xt y:$yt  type:${event.sensor.type}")
+            val v = binding.fab
+            v.x = v.left + xt * v.width * 2
+            v.y = v.top + yt * v.height * 2
+
+            if (xt.absoluteValue > BALANCE || yt.absoluteValue > BALANCE) {
+                anim1X.start()
+                anim1Y.start()
+            }
+        }
+
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
 
     }
 
