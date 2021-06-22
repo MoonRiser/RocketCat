44751ddc193991ccf7b28a250d4c469dc7f475b3
xres <xres331509@gmail.com>
2021-06-23 00:49:25 +0800

    刘海屏适配

--- app/src/main/java/com/example/rocketcat/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/rocketcat/ui/activity/SplashActivity.kt
@@ -26,23 +26,34 @@ class SplashActivity : BaseActivity<BaseViewModel, ActivitySplashBinding>() {
     private val imgs = arrayListOf(R.drawable.jump, R.drawable.paint, R.drawable.sit)
     private val adapter1 = MyGalleryAdapter(imgs)
 
-
     val evaluator = ArgbEvaluator()
 
-
     override fun layoutId() = R.layout.activity_splash
-
 
     override fun initView(savedInstanceState: Bundle?) {
 
 
-        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
             window.insetsController?.apply {
                 systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
                 hide(WindowInsets.Type.systemBars())
-
+                //侵入刘海屏
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                    window.attributes.layoutInDisplayCutoutMode =
+                        WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+                }
             }
+        } else {
+            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE
+                    // Set the content to appear under the system bars so that the
+                    // content doesn't resize when the system bars hide and show.
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    // Hide the nav bar and status bar
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN)
         }
         val colors = imgs.map { createPaletteSync(it)?.dominantSwatch?.rgb }
 
