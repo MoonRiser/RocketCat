481918be8b7fa4b521dbbb05c6558e6eec74f8e4
xres <xres331509@gmail.com>
2022-08-24 22:52:04 +0800

    更新依赖

--- app/src/main/java/com/example/rocketcat/ui/fragment/HomeViewModel.kt
+++ app/src/main/java/com/example/rocketcat/ui/fragment/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.rocketcat.ui.fragment
 
-import androidx.databinding.Observable
 import androidx.databinding.ObservableInt
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -8,10 +7,6 @@ import com.example.common.base.BaseViewModel
 import com.example.common.base.LoadStateFooterAdapter
 import com.example.rocketcat.ui.fragment.home_page.BookInfo
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.launch
 
 /**
@@ -52,23 +47,7 @@ class HomeViewModel : BaseViewModel() {
 
 
     init {
-        currentItem.addOnPropertyChangedCallback(object : Observable.OnPropertyChangedCallback() {
-            override fun onPropertyChanged(sender: Observable?, propertyId: Int) {
 
-            }
-        })
-        //test
-        viewModelScope.launch {
-            flowOf(1, 2, 3, 4, 3, 2, 1)
-                .map {
-                    delay(800)
-                    it
-                }.onCompletion { }
-                .collect {
-//                    currentItem.set(it)
-                }
-
-        }
 
     }
 }
